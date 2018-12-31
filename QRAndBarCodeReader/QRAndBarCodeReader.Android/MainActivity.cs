e0ea9586b3475abb4dea3ae961fa77d2a3e114da
ignotus87 <sajtosstangli@gmail.com>
2018-12-31 18:51:47 +0100

    initiali commit, add taking of photo

---
+++ QRAndBarCodeReader/QRAndBarCodeReader.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Plugin.Media;
+using Plugin.CurrentActivity;
+
+namespace QRAndBarCodeReader.Droid
+{
+    [Activity(Label = "QRAndBarCodeReader", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ QRAndBarCodeReader/QRAndBarCodeReader/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Plugin.Media;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QRAndBarCodeReader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var isCameraAvailable = await EnsurePermissions();
+
+            if (!isCameraAvailable)
+            {
+                return;
+            }
+
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
+        private async Task<bool> EnsurePermissions()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
