cdc0c6320dbdbc4898e40858bab554037587de1e
ignotus87 <sajtosstangli@gmail.com>
2018-12-31 22:59:00 +0100

    persist history across sessions

--- QRAndBarCodeReader/QRAndBarCodeReader/MainPage.xaml.cs
+++ QRAndBarCodeReader/QRAndBarCodeReader/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using ZXing.Net.Mobile.Forms;
@@ -9,16 +10,48 @@ namespace QRAndBarCodeReader
     public partial class MainPage : ContentPage
     {
         private ObservableCollection<string> _scanHistory = new ObservableCollection<string>();
+        private readonly string SCAN_HISTORY = "ScanHistory";
 
         public MainPage()
         {
             InitializeComponent();
+
+            if (Application.Current.Properties.ContainsKey(SCAN_HISTORY))
+            {
+                RestoreScanHistory();
+            }
+            else
+            {
+                Application.Current.Properties.Add(SCAN_HISTORY, "");
+                App.Current.SavePropertiesAsync();
+            }
 
             this.BindingContext = _scanHistory;
 
             CameraButton.Clicked += CameraButton_Clicked;
 
             Scan();
+        }
+
+        private async void SaveScanHistory()
+        {
+            Application.Current.Properties[SCAN_HISTORY] = string.Join("§§§", _scanHistory.ToList());
+            await App.Current.SavePropertiesAsync();
+        }
+
+        private void RestoreScanHistory()
+        {
+            _scanHistory = new ObservableCollection<string>((Application.Current.Properties[SCAN_HISTORY] as string ?? "").Split(new string[] { "§§§" }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        private void OnChange(object sender, EventArgs e)
+        {
+            Application.Current.Properties[SCAN_HISTORY] = _scanHistory;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
         }
 
         private async void OnItemTapped(object sender, ItemTappedEventArgs e)
@@ -59,6 +92,7 @@ namespace QRAndBarCodeReader
                     await Navigation.PopAsync();
                     //await DisplayAlert("Scanned Barcode", result.Text, "OK");
                     _scanHistory.Add(result.Text);
+                    SaveScanHistory();
                 });
             };
 
