627bf37bee7a30e96e3d8dd507e2cded42cbf6f1
ignotus87 <sajtosstangli@gmail.com>
2019-01-01 17:38:12 +0100

    store scan results in an object that determines type

---
+++ QRAndBarCodeReader/QRAndBarCodeReader/ScanResult.cs
@@ -0,0 +1,39 @@
+﻿namespace QRAndBarCodeReader
+{
+    public enum ScanResultType
+    {
+        Text,
+        Link,
+        Product
+    }
+
+    public class ScanResult
+    {
+        public string Text { get; private set; }
+        public ScanResultType Type { get; private set; }
+
+        public ScanResult(string text)
+        {
+            Text = text;
+
+            DetermineType();
+        }
+
+        private void DetermineType()
+        {
+            if (Text.StartsWith("http://") || Text.StartsWith("https://"))
+            {
+                Type = ScanResultType.Link;   
+            }
+            else if (Text.Length > 0 && char.IsNumber(Text[0]))
+            {
+                Type = ScanResultType.Product;
+            }
+            else
+            {
+                Type = ScanResultType.Text;
+            }
+            
+        }
+    }
+}
